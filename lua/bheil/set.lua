9e1583b8228fd17a7b0ba6891dbe04bfa142cfa9
BenHeil26 <BenHeil26>
2025-04-21 10:17:10 -0400

    set tabs to 2 spaces

--- lua/bheil/set.lua
+++ lua/bheil/set.lua
@@ -2,4 +2,9 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
 
