222ef7dec585c59ddf0e90f767fe836cf54561c1
BenHeil26 <BenHeil26>
2025-04-21 17:21:13 -0400

    configure autocomplete

--- lua/bheil/plugins/lsp.lua
+++ lua/bheil/plugins/lsp.lua
@@ -1,8 +1,13 @@
 return {
-  {'neovim/nvim-lspconfig', 
+  {'neovim/nvim-lspconfig',
     dependencies= {
       {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/nvim-cmp'}
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'hrsh7th/cmp-cmdline'},
+      {'hrsh7th/cmp-vsnip'},
+      {'hrsh7th/nvim-cmp'},
     },
     config = function()
       -- Reserve a space in the gutter
@@ -18,9 +23,66 @@ return {
         require('cmp_nvim_lsp').default_capabilities()
       )
 
-      require'lspconfig'.csharp_ls.setup{}
-      require'lspconfig'.gopls.setup{}
+      local cmp = require'cmp'
+      cmp.setup{
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+         end,
+        },
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' }, -- For vsnip users.
+       }, {
+            { name = 'buffer' },
+          })
+      }
+
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+            { name = 'cmdline' }
+          }),
+        matching = { disallow_symbol_nonprefix_matching = false }
+      })
+
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      require'lspconfig'.ts_ls.setup{
+        capabilities = capabilities
+      }
+      require'lspconfig'.csharp_ls.setup{
+        capabilities = capabilities
+      }
+      require'lspconfig'.gopls.setup{
+        capabilities = capabilities
+      }
       require('lspconfig').lua_ls.setup({
+        capabilities = capabilities,
         settings = {
           Lua = {
             telemetry = {
