a60fc1a2f5481f80d45b9d3ae1228a0c6dbf19af
BenHeil26 <BenHeil26>
2025-04-21 11:52:06 -0400

    add js support, redo keybinds

--- lua/bheil/plugins/lsp.lua
+++ lua/bheil/plugins/lsp.lua
@@ -1,88 +1,94 @@
 return {
-	{'neovim/nvim-lspconfig', 
-	dependencies= {
-		{'hrsh7th/cmp-nvim-lsp'},
-		{'hrsh7th/nvim-cmp'}
-	},
-	config = function()
-		-- Reserve a space in the gutter
-		-- This will avoid an annoying layout shift in the screen
-		vim.opt.signcolumn = 'yes'
+  {'neovim/nvim-lspconfig', 
+    dependencies= {
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/nvim-cmp'}
+    },
+    config = function()
+      -- Reserve a space in the gutter
+      -- This will avoid an annoying layout shift in the screen
+      vim.opt.signcolumn = 'yes'
 
-		-- Add cmp_nvim_lsp capabilities settings to lspconfig
-		-- This should be executed before you configure any language server
-		local lspconfig_defaults = require('lspconfig').util.default_config
-		lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-			'force',
-			lspconfig_defaults.capabilities,
-			require('cmp_nvim_lsp').default_capabilities()
-		)
-		require'lspconfig'.csharp_ls.setup{}
-		require'lspconfig'.gopls.setup{}
-		require('lspconfig').lua_ls.setup({
-				settings = {
-					Lua = {
-						telemetry = {
-							enable = false
-						},
-					},
-				},
-				on_init = function(client)
-					local join = vim.fs.joinpath
-					local path = client.workspace_folders[1].name
+      -- Add cmp_nvim_lsp capabilities settings to lspconfig
+      -- This should be executed before you configure any language server
+      local lspconfig_defaults = require('lspconfig').util.default_config
+      lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+        'force',
+        lspconfig_defaults.capabilities,
+        require('cmp_nvim_lsp').default_capabilities()
+      )
+      require'lspconfig'.javascript.setup {
+        cmd = { 'javascript-language-server', '--stdio' },
+        filetypes = { "javascript", "javascriptreact", "javascript.jsx" },
+        root_dir = function() return vim.loop.cwd() end
+      }
 
-					-- Don't do anything if there is project local config
-					if vim.uv.fs_stat(join(path, '.luarc.json')) 
-						or vim.uv.fs_stat(join(path, '.luarc.jsonc'))
-					then
-						return
-					end
+      require'lspconfig'.csharp_ls.setup{}
+      require'lspconfig'.gopls.setup{}
+      require('lspconfig').lua_ls.setup({
+        settings = {
+          Lua = {
+            telemetry = {
+              enable = false
+            },
+          },
+        },
+        on_init = function(client)
+          local join = vim.fs.joinpath
+          local path = client.workspace_folders[1].name
 
-					local nvim_settings = {
-						runtime = {
-							-- Tell the language server which version of Lua you're using
-							version = 'LuaJIT',
-						},
-						diagnostics = {
-							-- Get the language server to recognize the `vim` global
-							globals = {'vim'}
-						},
-						workspace = {
-							checkThirdParty = false,
-							library = {
-								-- Make the server aware of Neovim runtime files
-								vim.env.VIMRUNTIME,
-								vim.fn.stdpath('config'),
-							},
-						},
-					}
+          -- Don't do anything if there is project local config
+          if vim.uv.fs_stat(join(path, '.luarc.json')) 
+            or vim.uv.fs_stat(join(path, '.luarc.jsonc'))
+          then
+            return
+          end
 
-					client.config.settings.Lua = vim.tbl_deep_extend(
-						'force',
-						client.config.settings.Lua,
-						nvim_settings
-					)
-				end,
-			})
-		-- This is where you enable features that only work
-		-- if there is a language server active in the file
-		vim.api.nvim_create_autocmd('LspAttach', {
-			desc = 'LSP actions',
-			callback = function(event)
-				local opts = {buffer = event.buf}
+          local nvim_settings = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              version = 'LuaJIT',
+            },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = {'vim'}
+            },
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                -- Make the server aware of Neovim runtime files
+                vim.env.VIMRUNTIME,
+                vim.fn.stdpath('config'),
+              },
+            },
+          }
 
-				vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-				vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-				vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-				vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-				vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-				vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-				vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-				vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-				vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-				vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-			end,
-		})
-	end
-}
+          client.config.settings.Lua = vim.tbl_deep_extend(
+            'force',
+            client.config.settings.Lua,
+            nvim_settings
+          )
+        end,
+      })
+      -- This is where you enable features that only work
+      -- if there is a language server active in the file
+      vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP actions',
+        callback = function(event)
+          local opts = {buffer = event.buf}
+
+          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+          vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+          vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+          vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+          vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+          vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+          vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+          vim.keymap.set('n', '<leader>rr', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+          vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+          vim.keymap.set('n', '<leader>.', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        end,
+      })
+    end
+  }
 }
