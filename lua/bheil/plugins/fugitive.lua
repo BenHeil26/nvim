9b880fd1a1f6721ee1af643c849bf0274f25013c
BenHeil26 <BenHeil26>
2025-05-13 21:18:16 -0400

    update fugitive

--- lua/bheil/plugins/fugitive.lua
+++ lua/bheil/plugins/fugitive.lua
@@ -1,8 +1,13 @@
 return {
-    "tpope/vim-fugitive",
-    config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
-    end
+  "tpope/vim-fugitive",
+  config = function()
+    vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+    vim.keymap.set("n", "<leader>ga", "<cmd>:Git add .<CR>")
+    vim.keymap.set("n", "<leader>gc", function ()
+      local msg =  vim.fn.input("msg > ")
+      vim.cmd(":Git commit -m '" .. msg .. "'")
+    end)
+    vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+    vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+  end
 }
