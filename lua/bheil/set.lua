b1506c9f31ef26e2222d956121dd094e3446ba86
BenHeil26 <BenHeil26>
2025-06-16 10:46:05 -0400

    add autofmt support

--- lua/bheil/set.lua
+++ lua/bheil/set.lua
@@ -11,5 +11,16 @@ vim.o.scrolloff = 8
  
 vim.api.nvim_set_hl(0, "Visual", { bg = "cyan", fg = "none"})
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      callback = function()
+        vim.lsp.buf.format {async = false, id = args.data.client_id }
+      end,
+    })
+  end
+})
 
 
---
+++ test/test.go
@@ -0,0 +1,9 @@
+package test
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Hello World")
+}
