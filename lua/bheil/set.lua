412b2d3c6d0f8b80f357759a33ba8dd05abfebdf
BenHeil26 <BenHeil26>
2025-08-12 12:08:15 -0400

    set nowrap

--- lua/bheil/set.lua
+++ lua/bheil/set.lua
@@ -8,8 +8,9 @@ vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.termguicolors = true
 vim.o.scrolloff = 8
- 
-vim.api.nvim_set_hl(0, "Visual", { bg = "cyan", fg = "none"})
+vim.opt.wrap = false
+
+vim.api.nvim_set_hl(0, "Visual", { bg = "cyan", fg = "none" })
 
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("lsp", { clear = true }),
@@ -17,10 +18,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = args.buf,
       callback = function()
-        vim.lsp.buf.format {async = false, id = args.data.client_id }
+        vim.lsp.buf.format { async = false, id = args.data.client_id }
       end,
     })
   end
 })
-
-
