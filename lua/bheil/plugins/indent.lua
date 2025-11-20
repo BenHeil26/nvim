0d17c043d9222d9395a6a38d302b127c15d9f0c0
BenHeil26 <BenHeil26>
2025-11-20 12:54:30 -0500

    minor tweaks

--- lua/bheil/plugins/indent.lua
+++ lua/bheil/plugins/indent.lua
@@ -14,18 +14,17 @@ return {
       "RainbowViolet",
       "RainbowCyan",
     }
-
     local hooks = require "ibl.hooks"
     -- create the highlight groups in the highlight setup hook, so they are reset
     -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#b36a6c" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#b6a96a" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#6a8cae" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#b48a65" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#7a9c7a" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#9a7cae" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#6aa0a6" })
     end)
 
     require("ibl").setup { indent = { highlight = highlight } }
