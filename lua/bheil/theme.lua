10005e8acce5dfc8d07efc5230debfa71c07c06f
BenHeil26 <BenHeil26>
2025-12-11 12:14:22 -0500

    change theme to melange

--- lua/bheil/theme.lua
+++ lua/bheil/theme.lua
@@ -1,16 +1,1 @@
-require("cyberdream").setup({
-  transparent = true,
-  saturation = .6,
-  italic_comments = true,
-  hide_fillchars = true,
-  borderless_pickers = true,
-  highlights = {
-    -- Highlight groups to override, adding new groups is also possible
-    -- See `:h highlight-groups` for a list of highlight groups or run `:hi` to see all groups and their current values
-    Visual = { fg = "NONE", bg = "#8FD8DC", italic = false },
-    MatchParen = { fg = "#D16DFF", bg = "NONE", bold = true }
-  },
-
-})
-
-vim.cmd("colorscheme cyberdream")
+vim.cmd("colorscheme melange")
