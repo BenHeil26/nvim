b718724a10e2c35ae3bf66bc29345cb12cef63f2
Benji Heil <ben.heil.dev@gmail.com>
2025-12-31 22:21:19 -0500

    add feed.nvim

---
+++ lua/bheil/plugins/feed.lua
@@ -0,0 +1,50 @@
+return {
+  "neo451/feed.nvim",
+  dependencies = {
+    "gregorias/coop.nvim",
+  },
+  cmd = "Feed",
+  ---@module 'feed'
+  ---@type feed.config
+  opts = {
+    feeds = {
+      feeds = {
+        {
+          "https://neovim.io/news.xml",
+          name = "Neovim News",
+          tags = { "nvim", "news" }
+        },
+        {
+          "https://hnrss.org/frontpage",
+          name = "Hacker News Frontpage",
+          tags = { "tech", "news" }
+        },
+        {
+          "https://www.reddit.com/r/rust.rss",
+          name = "Rust Subreddit",
+          tags = { "rust", "reddit" }
+        },
+        {
+          "rsshub://github/trending/weekly/any",
+          name = "Trending Repos",
+          tags = { "trending", "github" }
+        },
+        {
+          "https://awsinsider.net/rss-feeds/news.aspx",
+          name = "AWS Insider",
+          tags = { "aws", "news" }
+        },
+        {
+          "https://feeds.feedburner.com/AmazonWebServicesBlog",
+          name = "AWS Blog",
+          tags = { "aws", "news" }
+        },
+        {
+          "https://golangweekly.com/rss/",
+          name = "Golang Weekly",
+          tags = { "go", "news" }
+        },
+      }
+    }
+  }
+}
