f9cec00f0dcb0df71476a1936ae9027c435a812e
Benji Heil <ben.heil.dev@gmail.com>
2025-12-31 19:47:14 -0500

    update integrations

--- lua/bheil/plugins/fidget.lua
+++ lua/bheil/plugins/fidget.lua
@@ -5,12 +5,7 @@ return {
     integration = {
       ["nvim-tree"] = {
         enable = true,              -- Integrate with nvim-tree/nvim-tree.lua (if installed)
-        -- DEPRECATED; use notification.window.avoid = { "NvimTree" }
       },
-      ["xcodebuild-nvim"] = {
-        enable = true,              -- Integrate with wojciech-kulik/xcodebuild.nvim (if installed)
-        -- DEPRECATED; use notification.window.avoid = { "TestExplorer" }
-      },
-    },   -- options
+    },   
   },
 }
