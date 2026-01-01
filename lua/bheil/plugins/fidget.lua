ac2f28b7ad71cd5e5858cf04552ec7d97441acd7
Benji Heil <ben.heil.dev@gmail.com>
2025-12-31 19:44:43 -0500

    update integrations

--- lua/bheil/plugins/fidget.lua
+++ lua/bheil/plugins/fidget.lua
@@ -1,6 +1,16 @@
 return {
   "j-hui/fidget.nvim",
   opts = {
-    -- options
+   -- Options related to integrating with other plugins
+    integration = {
+      ["nvim-tree"] = {
+        enable = true,              -- Integrate with nvim-tree/nvim-tree.lua (if installed)
+        -- DEPRECATED; use notification.window.avoid = { "NvimTree" }
+      },
+      ["xcodebuild-nvim"] = {
+        enable = true,              -- Integrate with wojciech-kulik/xcodebuild.nvim (if installed)
+        -- DEPRECATED; use notification.window.avoid = { "TestExplorer" }
+      },
+    },   -- options
   },
 }
--- test/rust/hello_world/target/debug/deps/hello_world-abfebda80ea37f90.d
+++
@@ -1,9 +0,0 @@
-/Users/bheil/.config/nvim/test/rust/hello_world/target/debug/deps/hello_world-abfebda80ea37f90.d: src/main.rs Cargo.toml
-
-/Users/bheil/.config/nvim/test/rust/hello_world/target/debug/deps/libhello_world-abfebda80ea37f90.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
