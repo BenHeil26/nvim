87ed6ecbd4afeaa835fa10ef7fc124b4e51cdac3
Benji Heil <ben.heil.dev@gmail.com>
2026-01-05 20:46:07 -0500

    add autotag for html style autopairing

--- lua/bheil/set.lua
+++ lua/bheil/set.lua
@@ -43,3 +43,14 @@ vim.opt.guicursor = {
   "o:hor50",
   "a:"
 }
+
+-- autotag options {{{
+require('nvim-ts-autotag').setup({
+  opts = {
+    -- Defaults
+    enable_close = true,          -- Auto close tags
+    enable_rename = true,         -- Auto rename pairs of tags
+    enable_close_on_slash = false -- Auto close on trailing </
+  },
+});
+-- }}}
