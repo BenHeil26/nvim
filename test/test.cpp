0a2dd57662aeef4d7ca7d88ae2a412f92d884600
Benji Heil <ben.heil.dev@gmail.com>
2026-05-26 17:59:48 -0400

    add c++ support and test

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,17 @@
+#include <cstdio>
 #include <iostream>
+using namespace std;
 
 class Foo {
   int val;
+  int val2;
+
+public:
+  string foo;
 };
+
+int main(int argc, char *argv[]) {
+  Foo *foo = new Foo();
+  foo->foo = "This is a string of characters";
+  printf("The value is '%s'\n", foo->foo.c_str());
+}
