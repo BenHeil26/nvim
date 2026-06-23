6227bcef4502545c58f1877085c37642bd9d4e71
Benji Heil <ben.heil.dev@gmail.com>
2026-06-22 23:05:12 -0400

    add

--- lua/bheil/plugins/smartmarks.lua
+++ lua/bheil/plugins/smartmarks.lua
@@ -1,6 +1,18 @@
 return {
   "BenHeil26/smartmarks.nvim",
   config = function()
-    require('smartmarks').setup({})
+    require('smartmarks').setup({
+      show_virtual = true,
+      float_options = {
+        relative = 'cursor',
+        row = 1,
+        col = 1,
+        anchor = "NW",
+        style = "minimal",
+        title = "smart_marks",
+        title_pos = "center",
+        border = "double",
+      }
+    })
   end
 }
